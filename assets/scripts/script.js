2360403773a48f714e0cdf254c517d60102d0d92
Chris.N <cenunez47@gmail.com>
2019-02-03 19:24:24 -0800

    Added howl.js and assigned .mp3 for each letter.

---
+++ assets/scripts/script.js
@@ -0,0 +1,6 @@
+//Random Color Generator
+
+// var r = Math.floor(Math.random() * 256);
+// var g = Math.floor(Math.random() * 256);
+// var b = Math.floor(Math.random() * 256);
+// var randomColor = 'rgb(' + r + ', ' + g + ', ' + b + ')'
